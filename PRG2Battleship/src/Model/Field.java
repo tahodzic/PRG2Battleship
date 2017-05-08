5e92de8b3e55863d30968e92601927feed7e61f0
Elia Grifo <elia@grifo.ch>
2017-05-08 16:23:22 +0200

    Merge origin/master
    Conflicts:
    	PRG2Battleship/src/Model/Ship.java

---
+++ PRG2Battleship/src/Model/Field.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author eliagrifo
+ */
+public class Field {
+    
+    public final int x;
+    public final int y;
+    
+    public Field(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+}
--- PRG2Battleship/src/Model/Ship.java
+++ PRG2Battleship/src/Model/Ship.java
@@ -7,8 +7,30 @@ package Model;
 
 /**
  *
- * @author Damir Hodzic
+ * @author Elia Grifo
  */
 public class Ship {
-    //test
+    
+    private final int length;
+    private boolean isAlive;
+    private Field[] field;
+    
+    public Ship(int length) {
+        this.isAlive = true;
+        this.length = length;
+        this.field = new Field[length];
+    }
+    
+    public void setField(int x, int y) {
+        
+    }
+    
+    public boolean isValidField() {
+        
+        return false;
+    }
+    
+    public void setIsAlive(boolean isAlive) {
+        this.isAlive = isAlive;
+    }
 }

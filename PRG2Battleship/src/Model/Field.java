9d0dd34d73ebc1c80dd222990a47b46c1e750940
Elia Grifo <elia@grifo.ch>
2017-05-09 12:57:18 +0200

    Merge origin/master
    Conflicts:
    	PRG2Battleship/src/Model/Ship.java

--- PRG2Battleship/src/Model/Field.java
+++ PRG2Battleship/src/Model/Field.java
@@ -5,18 +5,69 @@
  */
 package Model;
 
+import java.util.ArrayList;
+
 /**
  *
- * @author eliagrifo
+ * @author Elia Grifo
  */
 public class Field {
     
-    public final int x;
-    public final int y;
+    public int x;
+    public int y;
     
     public Field(int x, int y) {
+        setX(x);
+        setY(y);
+    }
+    
+    /**
+     * Compares if two fields are next to each other
+     * @return Returns true if fields are next to each other
+     */
+    public boolean isNextTo(Field field) {
+        if(field.x == this.x && (field.y+1 == this.y || field.y-1 == this.y)) {
+            return true;
+        } else if(field.y == this.y && (field.x+1 == this.x || field.x-1 == this.x)) {
+            return true;
+        }
+        return false;
+    }
+    
+    /**
+     * Compares if a field is next to a collection of fields
+     * @return Returns true if field is next to one of the input fields
+     */
+    public boolean isNextTo(ArrayList<Field> fields) {
+        for (Field field : fields) {
+            if(this.isNextTo(field)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    /**
+     * Compares if two fields are in the same line
+     * @return Returns true if fields are in the same line
+     */
+    public boolean isInSameLine(Field field) {
+        return (field.x == this.x || field.y == this.y);
+    }
+    
+    public void setX(int x) {
         this.x = x;
+    }
+    
+    public void setY(int y) {
         this.y = y;
     }
     
+    public int getX() {
+        return this.x;
+    }
+    
+    public int getY() {
+        return this.y;
+    }
 }
--- PRG2Battleship/src/Model/Ship.java
+++ PRG2Battleship/src/Model/Ship.java
@@ -5,6 +5,8 @@
  */
 package Model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Elia Grifo
@@ -13,24 +15,62 @@ public class Ship {
     
     private final int length;
     private boolean isAlive;
-    private Field[] field;
+    private final ArrayList<Field> fields;
     
     public Ship(int length) {
         this.isAlive = true;
         this.length = length;
-        this.field = new Field[length];
+        this.fields = new ArrayList<>();
     }
     
-    public void setField(int x, int y) {
-        
-    }
-    
-    public boolean isValidField() {
-        
+    /**
+     * Adds field to ship
+     * @return Returns true if the field was added successfully
+     */
+    public boolean addField(Field field) {
+        if(isFieldValid(field) && fields.size() < length) {
+            fields.add(field);
+            return true;
+        }
         return false;
+    }
+    
+    // TODO Es könnte noch ein schon benutztes
+    // Feld (anderes Schiff) gewählt werden
+    public boolean isFieldValid(Field field) {
+        int i = fields.size();
+        switch(i) {
+            case 0:
+                return true;
+            case 1:
+                if(fields.get(0).isNextTo(field)) {
+                    return true;
+                }
+            default:
+                if(field.isInSameLine(fields.get(0)) && field.isNextTo(fields)) {
+                    return true;
+                } else {
+                    return false;
+                }
+        }
     }
     
     public void setIsAlive(boolean isAlive) {
         this.isAlive = isAlive;
+    }
+    
+    public boolean getIsAlive() {
+        return this.isAlive;
+    }
+    
+    public static void main(String[] args) {
+        Ship ship = new Ship(2);
+        Field field1 = new Field(3,1);
+        Field field2 = new Field(3,2);
+        Field field3 = new Field(3,3);
+        ship.addField(field1);
+        ship.addField(field2);
+        ship.addField(field3);
+        System.out.println(ship.fields.size());
     }
 }
